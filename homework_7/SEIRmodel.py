0c148a2c5683809ce30c8feed8d55453c5efd06c
Jared Frazier <cscidev001@gmail.com>
2023-11-02 23:26:02 +0100

    doc to SEIRmodel

--- homework_7/SEIRmodel.py
+++ homework_7/SEIRmodel.py
@@ -1,21 +1,32 @@
-from numpy import ceil, arange
+from numpy import ceil, arange, ndarray
 from scipy.integrate import solve_ivp
 
-def SEIRmodel(R0, T, tau):
+def SEIRmodel(R0, T, tau) -> ndarray:
+    """Return SEIR model solution vector for tsteps in range(0, 500, 0.01)
+    
+    Args:
+        R0: Reproduction number.
+        T: Incubation period.
+        tau: Infectious period.
+
+    Returns:
+        ODE solutions ndarray of shape [len(tsteps), 4] where 
+        `S = solutions[:, 0]`, `E = solutions[:, 1]`, `I = solutions[:, 2]`, 
+        and `R = solutions[:, 3]`
+    """
     N = 1e5 # population size
     b = R0/tau
 
     # init conditions
-    E=0
-    I=10
-    R=0
-    S=N-E-I-R
+    E = 0
+    I = 10
+    R = 0
+    S= N - E - I - R
 
     # create tsteps at which the solution will be saved
     t0 = 0
     Tend=500
     dt=0.01
-    Nt = int(ceil(Tend/dt))
     tsteps_to_eval = arange(t0, Tend, dt)
 
     # ixs 0  1  2  3
