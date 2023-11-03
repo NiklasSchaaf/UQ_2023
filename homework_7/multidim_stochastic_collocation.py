ad22e16ff5ef7adfb1de5f98cf4b6be87f594f3e
Jared Frazier <cscidev001@gmail.com>
2023-11-03 12:47:47 +0100

    separated collocation matrix and tensor grid to two separate funcs

--- homework_7/multidim_stochastic_collocation.py
+++ homework_7/multidim_stochastic_collocation.py
@@ -3,20 +3,20 @@ import itertools
 from numpy import ndarray, cos, pi, zeros, array
 
 
-def dense_tensor_grid(k: int, d_dims: int) -> Tuple[ndarray, ndarray]:
-    """Return collocation nodes matrix and dense tensor grid at level `k`.
-
-    Uses extrema of Chebyshev polynomials for grid nodes.
+def get_collocation_nodes_matrix(k: int, d_dims: int) -> Tuple[ndarray, ndarray]:
+    """Return collocation nodes matrix using Curtis-Clenshaw nodes of level `k`.
 
     Example:
    
     >>> # Reproduce figure 7.1 from Xiu
     >>> # NOTE: k = 5 in figure, but to get 1089 points, k = 6 in this example
     >>> # ... i don't know why this is 
-    >>> from multidim_stochastic_collocation import dense_tensor_grid
+    >>> from multidim_stochastic_collocation import get_tensor_grid
+    >>> from multidim_stochastic_collocation import  get_collocation_nodes_matrix
     >>> import matplotlib.pyplot as plt 
     >>> k = 6; d = 2;
-    >>> collocation_nodes_matrix, tensor_grid = dense_tensor_grid(k, d)
+    >>> collocation_nodes_matrix = get_collocation_nodes_matrix(k, d)
+    >>> tensor_grid = get_tensor_grid(collocation_nodes_matrix)
     >>> plt.scatter(tensor_grid[0, :], tensor_grid[1, :], s = 1)
     >>> plt.show()
 
@@ -25,18 +25,17 @@ def dense_tensor_grid(k: int, d_dims: int) -> Tuple[ndarray, ndarray]:
         d_dims: Number of random variables in problem of interest.
 
     Returns:
-       Collocation nodes matrix  of shape `[d_dims, m_ik]` where 
-        `m_ik = 2**(k-1) + 1`, and a tensor grid of shape 
-        `[d_dims, m_ik**d_dims]` representing the cartesian product of row `i` 
-        in the collocation matrix with row `j` s.t. `i != j`.
-
+       Collocation nodes matrix of shape `[d_dims, m_ik]` where 
+        `m_ik = 2**(k-1) + 1`. Semantically, each random variable has
+        a vector of collocation nodes associated with it. This corresponds
+        to `collocation_nodes_matrix[random_variable_ix, :]`
+ 
     References:
         Xiu ch. 7.2.1 and 7.2.2.
     """
     assert k >= 1, "level of Clenshaw-Curtis grid must be >= 1"
     # The below algorithm is directly from Xiu equation 7.10 and desc. Xiu p. 82
-    # and builds the collocation nodes matrix (i.e., collocation nodes
-    # per dimension)
+    # and builds the collocation nodes matrix 
     m_ik = 2**(k-1) + 1 if k > 1 else 1 # num collocation nodes per dim
     collocation_nodes_matrix = zeros(shape=(d_dims, m_ik))
     for i in range(1, d_dims+1):
@@ -48,15 +47,28 @@ def dense_tensor_grid(k: int, d_dims: int) -> Tuple[ndarray, ndarray]:
             
             collocation_nodes_matrix[i-1, j-1] = Z_ij
 
-    # Create the tensor grid by taking a cartesian product of all rows in 
-    # the collocation matrix 
+    return collocation_nodes_matrix
+
+
+def get_tensor_grid(collocation_nodes_matrix: ndarray) -> ndarray: 
+    """Return dense tensor grid by cartesian product of collocation matrix rows.
+
+    Args:
+        collocation_nodes_matrix: Ndarray in `[d_dims, m_ik]`.
+
+    Returns:
+        Tensor grid in `[d_dims, m_ik**d_dims]`.
+
+    References:
+        See Xiu figure 7.1.
+    """
+    d_dims = collocation_nodes_matrix.shape[0]
     tensor_grid = array(
         tuple(
             itertools.product(
                 *[collocation_nodes_matrix[i, :] for i in range(d_dims)]))).T
+    return tensor_grid
 
-    return collocation_nodes_matrix, tensor_grid
-   
  
 def nested_clenshaw_curtis_node(j, m_ik):
     """Extrema of Chebyshev polynomial.
