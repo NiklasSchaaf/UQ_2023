ade5186025419c05bb569f7a1a74a4b171490c42
Jared Frazier <cscidev001@gmail.com>
2023-11-05 17:55:06 +0100

    init working multidimensional stochastic collocation

--- homework_7/multidim_stochastic_collocation.py
+++ homework_7/multidim_stochastic_collocation.py
@@ -1,6 +1,24 @@
-from typing import List, Union, Callable
+"""Functions for performing multidimensional stochastic collocation"""
+
+
+from typing import List, Union, Callable, Tuple
+
 import itertools 
-from numpy import ndarray, cos, pi, zeros, array
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+from numpy import ndarray, cos, pi, zeros, array, zeros_like
+from numpy.random import uniform, beta
+
+from tqdm import tqdm
+
+# explicitly add project root dir to path to fix import issue
+sys.path.append(os.path.join(os.path.dirname(__file__), '..')) 
+from homework_7.SEIRmodel import SEIRmodel
+
 
 def get_clenshawcurtis_collocation_nodes_matrix(k: int, d_dims: int) -> ndarray:
     """Matrix where rows are collocation nodes for the i^th random variable.
@@ -109,13 +127,27 @@ def lagrange_basis_product(js, Zs, collocation_nodes_matrix):
         Z_n = Zs[n]
         j_n = js[n]
         theta_M_n = collocation_nodes_matrix[n, :]
-        prod *= lagrange_basis(Z_n, theta_M_n, n)
+        prod *= lagrange_basis(Z_n, theta_M_n, j_n)
     return prod
 
 
-def map_val_to_new_interval(val, a, b):
-    """Map value on interval [-1, 1] to [a, b]."""
+def map_uniform_val_to_new_interval(val, a, b):
+    """Map value on interval [-1, 1] to [a, b].
+
+    References:
+        Ch. 9.2.1 from "Uncertainty Quantification and Predictive Computational 
+        Science" (McClarren 2018).
+    """
     return ((b-a)/2)*val + (a + b)/2
+
+
+def map_beta_val_to_new_interval(val, a, b):
+    """Map value on beta interval [0, 1] to [a, b]
+    
+    References:
+        [Difference between standard beta and unstandard beta distributions?](https://stats.stackexchange.com/questions/186465/difference-between-standard-beta-and-unstandard-beta-distributions/186467#186467)
+    """
+    return val*(b-a) + a
 
 
 def stochastic_collocation_summand(
@@ -143,46 +175,13 @@ def stochastic_collocation_summand(
     """
     d_dims = len(js)
     collocation_nodes_at_j = collocation_nodes_matrix[range(len(js)), js]
-    if isinstance(model, Callable):
-        u = model(*collocation_nodes_at_j)
-    elif isinstance(model, ndarray):
-        u = model
-    else:
-        raise TypeError(
-            "`model` must be of type `ndarray` or `Callable`" 
-            f"but got {type(model)}")
+
+    # Determine function u evaluation at collocation nodes from multi-index js
+    u = model(*collocation_nodes_at_j)
+
     L = lagrange_basis_product(js, Zs, collocation_nodes_matrix)
+
     return u*L
-
-
-def multidim_stochastic_collocation(
-    Zs: ndarray, 
-    model: Union[Callable, ndarray], 
-    collocation_nodes_matrix: ndarray):
-    """Multidimensional stochastic collocation using dense tensor product.
-   
-    Args: 
-        Zs: Vector of realizations of the random variables 
-            (i.e., Z_1, Z_2, ..., Z_d).
-        collocation_nodes_matrix: Matrix of collocation nodes where the i^th
-            row is the set of collocation nodes for the i^th random variable.
-        model: Function `u` that will be evaluated at collocation nodes 
-            corresponding to the indices `js`.
-
-    Returns:
-        pass
-
-    References:
-        Slide 9 from UQ lecture 8.
-    """
-    multi_index = get_multi_index(collocation_nodes_matrix)
-    u_approx = 0 # need to change this based on model output shape
-
-    for js in multi_index:
-        u_approx += stochastic_collocation_summand(
-            js, Zs, collocation_nodes_matrix)
-
-    return u_approx
 
 
 def get_multi_index(collocation_nodes_matrix: ndarray) -> itertools.product:
@@ -223,3 +222,109 @@ def get_multi_index(collocation_nodes_matrix: ndarray) -> itertools.product:
 
     return multi_index
 
+
+def multidim_stochastic_collocation(
+    Zs: ndarray, 
+    model: Callable, 
+    collocation_nodes_matrix: ndarray,
+    multi_index: Tuple):
+    """Multidimensional stochastic collocation using dense tensor product.
+   
+    Args: 
+        Zs: Vector of realizations of the random variables 
+            (i.e., Z_1, Z_2, ..., Z_d).
+        collocation_nodes_matrix: Matrix of collocation nodes where the i^th
+            row is the set of collocation nodes for the i^th random variable.
+        model: Function `u` that will be evaluated at collocation nodes 
+            corresponding to the indices `js`.
+        multi_index: Tuple of indices `js` used for the summation over
+            a d-dimensional `Zs` vector.
+
+    Returns:
+        Multidimensional stochastic collocation approximation of `model`
+        using random variables `Zs` and `collocation_nodes_matrix`.
+
+    References:
+        Slide 9 from UQ lecture 8.
+    """
+    # uses a dummy evaluation of the model to ascertain its output shape
+    u_approx = zeros_like(model(*Zs))
+
+    # iterate through multi-indices and sum using stochastic collocation
+    for js in tqdm(multi_index, desc="Performing stochastic collocation"):
+        u_approx += stochastic_collocation_summand(
+            js, Zs, collocation_nodes_matrix, model)
+
+    return u_approx
+
+
+
+if __name__ == "__main__":
+    ## Test stochastic collocation function ##
+    
+    # define random variable intervals 
+    R0_interval = [1.5, 3.0] # ~ Uniform distribution   
+    T_interval = [5, 10]     # ~ Uniform distribution
+    tau_interval = [1, 14]   # ~ Beta distribution  
+    beta_a = beta_b = 2      # parameters of beta distribution
+ 
+    # Create the Zs vector by selecting arbitrary values in the appropriate
+    # intervals for the random vars
+    R0 = 2.2
+    T = 9
+    tau = 10
+    Zs = [R0, T, tau] # this order is important for the model
+   
+    # Create the collocation nodes 
+    # NOTE: The clenshaw curtis level determines how many collocation
+    # nodes are used... and since the number of points in the tensor grid
+    # is `num_nodes**d`, this grows exponentially fast and is a huge bottleneck
+    # e.g., level 5 --> 17 collocation nodes and d=3 --> 17**3 = 4913 
+    # evaluations of model function `u`
+    clenshaw_curtis_level = 3  
+    collocation_nodes_matrix = get_clenshawcurtis_collocation_nodes_matrix(
+        k=clenshaw_curtis_level, d_dims=len(Zs)) 
+
+    # Scale the collocation nodes [-1, 1] -> [a, b] for respective random vars
+    n_nodes_per_randvar = collocation_nodes_matrix.shape[1]
+
+    collocation_nodes_matrix[0, :] = array( # scale R0 nodes
+        [map_uniform_val_to_new_interval(
+            collocation_nodes_matrix[0, i], *R0_interval) 
+            for i in range(n_nodes_per_randvar)]) 
+
+    collocation_nodes_matrix[1, :] = array( # scale T nodes
+        [map_uniform_val_to_new_interval(
+            collocation_nodes_matrix[1, i], *T_interval) 
+            for i in range(n_nodes_per_randvar)])
+
+    collocation_nodes_matrix[2, :] = array( # scale tau nodes
+        [map_uniform_val_to_new_interval(
+            collocation_nodes_matrix[2, i], *tau_interval) 
+            for i in range(n_nodes_per_randvar)])
+
+    # Compute the multi-index 
+    multi_index = tuple(get_multi_index(collocation_nodes_matrix))
+
+    # perform stochastic collocation
+    u_approx = multidim_stochastic_collocation(
+        Zs, SEIRmodel, collocation_nodes_matrix, multi_index)
+    
+    # get the approximated infected from the stochastic collocation procedure
+    infected_approx = u_approx[:, 2] 
+
+    # get the infected from just running the model as is
+    infected = SEIRmodel(*Zs)[:, 2]
+    
+    # plot the approximated infected and the regular model infected
+    plt.plot(infected_approx, label="Stochastic Collocation")
+    plt.plot(infected, label="Regular")
+
+    plt.xlabel("timesteps")
+    plt.ylabel("infected")
+    plt.title("Multidimensional Stochastic Collocation" 
+    f" of {len(multi_index)} Points and Regular Model"
+    f"\n[R0, t, tau] -> {Zs}")
+    plt.legend()
+
+    plt.show()
